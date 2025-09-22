8108513fabaa0cfa3df9a18e122de2164f7fd743
FoxLovesYou <komak57@gmail.com>
2025-09-22 17:02:57 -0400

    Removed init_sig from SetRoomDataInternal

--- Core/HLE/sceNp2.cpp
+++ Core/HLE/sceNp2.cpp
@@ -1265,9 +1265,9 @@ static int sceNpMatching2SetRoomDataInternal(int ctxId, u32 reqParamPtr, u32 opt
 		if ((ret = npServer->SetRoomDataInternal(req)) != 0)
 			return notifyRequestHandler(request_id, SCE_NP_MATCHING2_REQUEST_EVENT_SetRoomDataExternal, hleLogError(Log::sceNet, ret), 0);
 
-		auto [r, self] = npServer->GetSelf(req->roomId);
+		/*auto [r, self] = npServer->GetSelf(req->roomId);
 		if (r == 0)
-			g_signaling.init_sig(self->userInfo.npId);
+			g_signaling.init_sig(self->userInfo.npId);*/
 
 		return notifyRequestHandler(request_id, SCE_NP_MATCHING2_REQUEST_EVENT_SetRoomDataExternal, SCE_NP_MATCHING2_OKAY, 0);
 	});
