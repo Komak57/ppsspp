ca8936817c94cabe74cf6f859fe848ddd974069f
FoxLovesYou <komak57@gmail.com>
2025-11-04 17:43:51 -0500

    Added GetServers back to PSN Auth Agent for whoever comes back to reverse engineer PSN Networking

--- Core/Net/NPAgents/PSNAuthAgent.cpp
+++ Core/Net/NPAgents/PSNAuthAgent.cpp
@@ -175,4 +175,124 @@ namespace net {
 		}
 		return false;
 	}
+
+	int PSNAuthAgent::GetServers(SceNpCommunicationId npTitleId) {
+		Url url("http://static-resource.np.community.playstation.net/np/resource/psp-title/" + std::string(npTitleId.data) + "_00/matching/" + std::string(npTitleId.data) + "_00-matching.xml");
+		http::Client client(&ProcessHostnameWithInfraDNS);
+		bool cancelled = false;
+		net::RequestProgress progress(&cancelled);
+		if (!client.Resolve(url.Host().c_str(), url.Port())) {
+			return hleLogError(Log::sceNet, SCE_NP_COMMUNITY_SERVER_ERROR_NO_SUCH_TITLE, "HTTP failed to resolve %s", url.Resource().c_str());
+		}
+
+		client.SetDataTimeout(20.0);
+		if (client.Connect()) {
+			char requestHeaders[4096];
+			snprintf(requestHeaders, sizeof(requestHeaders),
+				"User-Agent: PS3Community-agent/1.0.0 libhttp/1.0.0\r\n");
+
+			DEBUG_LOG(Log::sceNet, "GET URI: %s", url.ToString().c_str());
+			http::RequestParams req(url.Resource(), "*/*");
+			int err = client.SendRequest("GET", req, requestHeaders, &progress);
+			if (err < 0) {
+				client.Disconnect();
+				return hleLogError(Log::sceNet, SCE_NP_COMMUNITY_SERVER_ERROR_FORBIDDEN, "HTTP GET Error = %d", err);
+			}
+
+			core::Buffer readbuf;
+			std::vector<std::string> responseHeaders;
+			int code = readbuf.ReadHTML(client.sock(), nullptr);
+			//int code = client.ReadResponse(&readbuf, &progress);
+			if (code != 200) {
+				client.Disconnect();
+				return hleLogError(Log::sceNet, SCE_NP_COMMUNITY_SERVER_ERROR_INTERNAL_SERVER_ERROR, "HTTP Error Code = %d", code);
+			}
+			client.Disconnect();
+
+			std::string entity;
+			size_t readBytes = readbuf.size();
+			readbuf.Take(readBytes, &entity);
+
+			INFO_LOG(Log::sceNet, "Entity Data: %d", entity);
+
+			// TODO: Use XML Parser to get the Tag and it's attributes instead of searching for keywords on the string
+			std::string text;
+			size_t ofs = entity.find("titleid=");
+			if (ofs == std::string::npos)
+				return hleLogError(Log::sceNet, SCE_NP_COMMUNITY_SERVER_ERROR_NO_SUCH_TITLE, "titleid not found");
+
+			ofs += 9;
+			size_t ofs2 = entity.find('"', ofs);
+			text = entity.substr(ofs, ofs2 - ofs);
+			INFO_LOG(Log::sceNet, "%s - Title ID: %s", __FUNCTION__, text.c_str());
+
+			int i = 1;
+			while (true) {
+				NPServerInfo server{};
+				server.nptype = NPAgentType::PSN;
+
+				ofs = entity.find("<agent-fqdn", ++ofs2);
+				if (ofs == std::string::npos) {
+					if (i == 1)
+						return hleLogError(Log::sceNet, SCE_NP_COMMUNITY_SERVER_ERROR_NO_SUCH_TITLE, "agent-fqdn not found");
+					else
+						break;
+				}
+
+				size_t frontPos = ++ofs;
+				ofs = entity.find("id=", frontPos);
+				if (ofs == std::string::npos)
+					return hleLogError(Log::sceNet, SCE_NP_COMMUNITY_SERVER_ERROR_NO_SUCH_TITLE, "agent id not found");
+
+				ofs += 4;
+				ofs2 = entity.find('"', ofs);
+				text = entity.substr(ofs, ofs2 - ofs);
+				server.id = std::stoi(text.c_str());
+				INFO_LOG(Log::sceNet, "%s - Agent-FQDN#%d ID: %s", __FUNCTION__, i, text.c_str());
+
+				ofs = entity.find("port=", frontPos);
+				if (ofs == std::string::npos)
+					return hleLogError(Log::sceNet, SCE_NP_COMMUNITY_SERVER_ERROR_NO_SUCH_TITLE, "agent port not found");
+
+				ofs += 6;
+				ofs2 = entity.find('"', ofs);
+				text = entity.substr(ofs, ofs2 - ofs);
+				server.port = std::stoi(text.c_str());
+				INFO_LOG(Log::sceNet, "%s - Agent-FQDN#%d Port: %s", __FUNCTION__, i, text.c_str());
+
+				ofs = entity.find("status=", frontPos);
+				if (ofs == std::string::npos)
+					return hleLogError(Log::sceNet, SCE_NP_COMMUNITY_SERVER_ERROR_NO_SUCH_TITLE, "agent status not found");
+
+				ofs += 8;
+				ofs2 = entity.find('"', ofs);
+				text = entity.substr(ofs, ofs2 - ofs);
+				std::string alive = { 'a','l','i','v','e' };
+				server.status = SCE_NP_MATCHING2_SERVER_STATUS_UNAVAILABLE;
+				if (text == alive)
+					server.status = SCE_NP_MATCHING2_SERVER_STATUS_AVAILABLE;
+				INFO_LOG(Log::sceNet, "%s - Agent-FQDN#%d Status: %s", __FUNCTION__, i, text.c_str());
+
+				ofs = entity.find('>', ++ofs2);
+				if (ofs == std::string::npos)
+					return hleLogError(Log::sceNet, SCE_NP_COMMUNITY_SERVER_ERROR_NO_SUCH_TITLE, "agent host not found");
+
+				ofs2 = entity.find("</agent-fqdn", ++ofs);
+				text = entity.substr(ofs, ofs2 - ofs);
+				server.host = text;
+				INFO_LOG(Log::sceNet, "%s - Agent-FQDN#%d Host: %s", __FUNCTION__, i, text.c_str());
+
+				if (!npServer)
+					npServer = net::CreateNPAgent(NPAgentType::PSN, server.host, server.port);
+				npServer->servers.push_back(server);
+				//serversPtr->emplace(server_id, net::CreateNPAgent(net::NPAgentType::PSN, server_id, server_host, server_port, server_status));
+				i++;
+			}
+		}
+		return 0;
+	}
+
+	int PSNAuthAgent::GetWorldInfo(SceNpMatching2ContextId ctxId, SceNpMatching2RequestId reqId, int server_id, SceNpCommunicationId npCommId) {
+		return 0;
+	}
 }
