1d7ac57273cb4acd2571c34b1f41328b80e29969
FoxLovesYou <komak57@gmail.com>
2026-04-18 14:51:16 -0400

    Fixed void compile bug

--- Core/Screenshot.cpp
+++ Core/Screenshot.cpp
@@ -342,7 +342,7 @@ ScreenshotResult TakeGameScreenshot(Draw::DrawContext *draw, const Path &filenam
 	}
 
 	if (callback) {
-		g_threadManager.EnqueueTask(new IndependentTask<std::function<void()>>(TaskType::IO_BLOCKING, TaskPriority::LOW,
+		g_threadManager.EnqueueTask(new IndependentTask(TaskType::IO_BLOCKING, TaskPriority::LOW,
 			[buf = std::move(buf), callback = std::move(callback), filename, fmt, w, h]() {
 			u8 *flipbuffer = nullptr;
 			u32 width = w, height = h;
