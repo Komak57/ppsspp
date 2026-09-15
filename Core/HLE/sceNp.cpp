4f56f0e3594abdf832c4eed88209d0f782857a1e
FoxLovesYou <komak57@gmail.com>
2026-09-15 12:31:37 -0400

    Changed default user_id to 0 to indicate a STUN request, instead of NAT maintanance

--- Core/HLE/sceNp.cpp
+++ Core/HLE/sceNp.cpp
@@ -79,7 +79,7 @@ void __NpInit() {
 
 	memcpy(&online_name.data, "DummyOnlineId", 14);
 	memcpy(&avatar_url.data, "http://DummyAvatarUrl", 22);
-	user_id.store(1); // cannot be zero - will be provided by the NPServer login
+	user_id.store(0); // Zero indicates no-linked-user - will be update by the NPServer login
 }
 void __NpShutdown() {
 	if (npAuthServer && npAuthServer->IsConnected())
